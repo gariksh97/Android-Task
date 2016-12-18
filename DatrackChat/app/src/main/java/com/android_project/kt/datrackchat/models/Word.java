1211744bc6bac1207b222e06e050dc70ee4f00a7
kekcik <ivan@trofimov.mobi>
2016-12-19 01:38:18 +0300

    add models

---
+++ DatrackChat/app/src/main/java/com/android_project/kt/datrackchat/models/Word.java
@@ -0,0 +1,14 @@
+package com.android_project.kt.datrackchat.models;
+
+/**
+ * Created by ivan on 19.12.16.
+ */
+
+public class Word {
+    String nativeWord;
+    String russianWord;
+    public Word (String nativeWord, String russianWord) {
+        this.nativeWord = nativeWord;
+        this.russianWord = russianWord;
+    }
+}
