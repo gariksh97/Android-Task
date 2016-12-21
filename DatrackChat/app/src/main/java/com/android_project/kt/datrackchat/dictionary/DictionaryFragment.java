513820265997e166e36cd381c5efd92aa4db0276
iKefir <danshk@mail.ru>
2016-12-21 09:18:08 +0300

    Minor fix

--- DatrackChat/app/src/main/java/com/android_project/kt/datrackchat/dictionary/DictionaryFragment.java
+++ DatrackChat/app/src/main/java/com/android_project/kt/datrackchat/dictionary/DictionaryFragment.java
@@ -69,9 +69,7 @@ public class DictionaryFragment extends Fragment {
         if (dictionary == null) dictionary = manager.getWholeDictionary(
                 (MainActivity) getActivity()
         );
-        if (adapter == null) {
-            adapter = new WordRecyclerAdapter(getContext(), dictionary);
-        }
+        adapter = new WordRecyclerAdapter(getContext(), dictionary);
         recyclerView.setAdapter(adapter);
         EditText editText = (EditText) rootView.findViewById(R.id.search_edit);
         editText.addTextChangedListener(
